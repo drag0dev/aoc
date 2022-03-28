cb56fdd4b236750a676fc01c03a789a52faff3e8
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-03-28 17:16:02 +0200

    2021/day4

--- 2021/day4/main.go
+++ 2021/day4/main.go
@@ -7,90 +7,116 @@ import (
 	"strings"
 )
 
-func caclulateRes(ticket string)(int){
-    var res int = 0
-
-    ticket = strings.ReplaceAll(ticket, "\n", " ")
-
-    for _, number := range strings.Split(ticket, " "){
-        if !strings.Contains(number, "F"){
-            num, _ := strconv.Atoi(number)
-            res += num
+func checkIfBingoWinning(bingo *string)string{
+    bingoSplit := strings.Split(*bingo, "\n\n")
+    // by rows
+    // split into bingos
+    for index, bingo := range bingoSplit{
+        // split each bingo into lines
+        lineSplit := strings.Split(bingo, "\n")
+        for _, line := range lineSplit{
+            if strings.Count(line, "F") == 5{
+                fmt.Printf("(%d) ", index)
+                return bingoSplit[index]
+            }
         }
     }
-    return res
+
+    //by columns
+    var foundNumsColumn [5]int
+    for outterIndex, bingo := range bingoSplit{
+        for _, line := range strings.Split(bingo, "\n"){
+            if len(line) == 0 {
+                continue
+            }
+            line = strings.ReplaceAll(line, "  ", " ")
+            if line[0] == ' '{
+                line = strings.Replace(line, " ", "", 1)
+            }
+            lineSplit := strings.Split(line, " ")
+            for index, num := range lineSplit{
+                if strings.Index(num, "F") != -1 {
+                    foundNumsColumn[index]++
+                }
+            }
+
+            for index, found := range foundNumsColumn{
+                if found == 6 {
+                    fmt.Printf("(%d) ", outterIndex)
+                    return bingoSplit[outterIndex]
+                }else{
+                    foundNumsColumn[index] = 0
+                }
+            }
+        }
+    }
+
+    return ""
 }
 
-// garbage slow solution
-func main(){
-    content, _ := ioutil.ReadFile("input.txt")
-    formatedData := strings.Split(string(content), "\n\n")
-
-    tickets := strings.Replace(string(content), formatedData[0]+"\n", "", 1)
-    var drawnNumbersStr []string = strings.Split(formatedData[0], ",")
-    var drawnNumbers []int
+func calculateWinningBingo(bingo string, drawnNumber int)(int){
     var sum int
 
-    for _, ker := range drawnNumbersStr{
-        number, _ := strconv.Atoi(ker)
-        drawnNumbers = append(drawnNumbers, number)
+    for _, line := range strings.Split(bingo, "\n"){
+        if len(line) == 0 {
+            continue
+        }
+        line = strings.ReplaceAll(line, "  ", " ")
+        if line[0] == ' '{
+            line = strings.Replace(line, " ", "", 1)
+        }
+
+        for _, num := range strings.Split(line, " "){
+            if strings.Index(num, "F") == -1{
+                parseNum, _ := strconv.ParseInt(num, 10, 64)
+                sum += int(parseNum)
+            }
+        }
     }
+
+    return sum * drawnNumber
+}
+
+func markBingos(bingos *string, number string){
+    if len(number) == 1{
+        *bingos = strings.ReplaceAll(*bingos, fmt.Sprintf(" %s ", number), fmt.Sprintf("F%s ", number))
+        *bingos = strings.ReplaceAll(*bingos, fmt.Sprintf(" %s\n", number), fmt.Sprintf("F%s\n", number))
+        *bingos = strings.ReplaceAll(*bingos, fmt.Sprintf(" %s ", number), fmt.Sprintf("F%s ", number))
+    }else {
+        *bingos = strings.ReplaceAll(*bingos, fmt.Sprintf("%s ", number), fmt.Sprintf("F%s ", number))
+        *bingos = strings.ReplaceAll(*bingos, fmt.Sprintf(" %s\n", number), fmt.Sprintf(" F%s\n", number))
+    }
+}
+
+func main(){
+    contents, _ := ioutil.ReadFile("input.txt")
+    bingos := strings.Split(string(contents), "\n")
+
+    var drawnNumbers []string = strings.Split(bingos[0], ",")
+    var bingoNo int
+    bingos = bingos[1:]
+    joinedBingos := strings.Join(bingos, "\n")
 
     for _, drawnNumber := range drawnNumbers{
-        if drawnNumber < 10 {
-            fmt.Print("manji")
-            tickets = strings.Replace(tickets, fmt.Sprintf("\n %d ", drawnNumber), fmt.Sprintf("\n%dF ", drawnNumber), 1)
-            tickets = strings.Replace(tickets, fmt.Sprintf("  %d ", drawnNumber), fmt.Sprintf(" %dF ", drawnNumber), 1)
-            tickets = strings.Replace(tickets, fmt.Sprintf("  %d\n", drawnNumber), fmt.Sprintf(" %dF\n", drawnNumber), 1)
-        } else{
-            tickets = strings.Replace(tickets, fmt.Sprintf("\n%d", drawnNumber), fmt.Sprintf("\n%dF", drawnNumber), 1)
-            tickets = strings.Replace(tickets, fmt.Sprintf(" %d ", drawnNumber), fmt.Sprintf(" %dF ", drawnNumber), 1)
-            tickets = strings.Replace(tickets, fmt.Sprintf(" %d\n", drawnNumber), fmt.Sprintf(" %dF\n", drawnNumber), 1)
-        }
-
-        //check if there is a winning ticket
-        for _, ticket := range strings.Split(tickets, "\n\n"){
-            var ticketLines []string = strings.Split(ticket, "\n")
-
-            // rows
-            for _, ticketLine := range ticketLines{
-                if(strings.Count(ticketLine, "F") == 5){
-                    fmt.Println("Winning number:", drawnNumber)
-                    sum = caclulateRes(ticket) * drawnNumber
-                    break
+        markBingos(&joinedBingos, drawnNumber)
+        drawnNumberInt, _ := strconv.ParseInt(drawnNumber, 10, 64)
+        var found bool
+        for{
+            winningBingo := checkIfBingoWinning(&joinedBingos)
+            if winningBingo == ""{
+                fmt.Println("no winning")
+                if found{
+                    return
                 }
-            }
-
-            // columns
-            var numOfFs [6]int
-            for _, line := range ticketLines{
-                for index, number := range strings.Split(strings.ReplaceAll(line, "  ", " "), " "){
-                    if strings.Contains(number, "F"){
-                        numOfFs[index] = numOfFs[index]+1
-                    }
-                }
-
-                // check if there is a winning ticket
-                for _, line := range numOfFs{
-                    if line == 5 {
-                        sum = caclulateRes(ticket)
-                        break
-                    }
-                }
-
-            }
-
-            if sum != 0{
                 break
+            }else if winningBingo != ""{
+                fmt.Printf("(%s) %d. bingo winning: %d\n",drawnNumber,bingoNo+1, calculateWinningBingo(winningBingo, int(drawnNumberInt)))
+                fmt.Println(winningBingo)
+                joinedBingos = strings.ReplaceAll(joinedBingos, winningBingo, "")
+                joinedBingos = strings.ReplaceAll(joinedBingos, "\n\n\n", "\n\n")
+                bingoNo++
+                found = true
             }
-
-        }
-        if sum != 0{
-            break
         }
     }
-
-    fmt.Println(tickets)
-
-    fmt.Println(sum)
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advent of code 2021
 done in golang
+
+## 2021
+* day 4, part two not getting correct results
